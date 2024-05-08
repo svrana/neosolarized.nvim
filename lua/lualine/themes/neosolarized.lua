ce91523ab50cccb7da7e227efc7c1084e84f10b5
Shaw Vrana <shaw@vranix.com>
2024-05-08 14:10:38 -0700

    lualine: use the solarized-dark theme from lualine
    Almost exactly the same afaict except for the slight difference
    in how the directory is specified in nvim-tree which was
    a green color before this change.

--- lua/lualine/themes/neosolarized.lua
+++ lua/lualine/themes/neosolarized.lua
@@ -1,45 +1,38 @@
+-- Copyright (c) 2020-2021 shadmansaleh
+-- MIT license, see LICENSE for more details.
+-- Credit: itchyny(lightline)
+-- stylua: ignore
 local colors = {
-  black        = '#002b36',
-  white        = '#93a1a1',
-  red          = '#dc322f',
-  green        = '#859900',
-  blue         = '#268bd2',
-  yellow       = '#b58900',
-  gray         = '#839496',
-  darkgray     = '#073642',
-  lightgray    = '#586e75',
-  inactivegray = '#073642',
+  base03  =  '#002b36',
+  base02  =  '#073642',
+  base01  =  '#586e75',
+  base00  =  '#657b83',
+  base0   =  '#839496',
+  base1   =  '#93a1a1',
+  base2   =  '#eee8d5',
+  base3   =  '#fdf6e3',
+  yellow  =  '#b58900',
+  orange  =  '#cb4b16',
+  red     =  '#dc322f',
+  magenta =  '#d33682',
+  violet  =  '#6c71c4',
+  blue    =  '#268bd2',
+  cyan    =  '#2aa198',
+  green   =  '#859900',
 }
 
 return {
   normal = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.gray, fg = colors.black},
-    c = {bg = colors.darkgray, fg = colors.white}
+    a = { fg = colors.base03, bg = colors.blue, gui = 'bold' },
+    b = { fg = colors.base03, bg = colors.base1 },
+    c = { fg = colors.base1, bg = colors.base02 },
   },
-  insert = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.gray, fg = colors.black},
-    c = {bg = colors.darkgray, fg = colors.white}
-  },
-  visual = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.gray, fg = colors.black},
-    c = {bg = colors.darkgray, fg = colors.white}
-  },
-  replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.gray, fg = colors.black},
-    c = {bg = colors.darkgray, fg = colors.white}
-  },
-  command = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.gray, fg = colors.black},
-    c = {bg = colors.darkgray, fg = colors.white}
-  },
+  insert = { a = { fg = colors.base03, bg = colors.green, gui = 'bold' } },
+  visual = { a = { fg = colors.base03, bg = colors.magenta, gui = 'bold' } },
+  replace = { a = { fg = colors.base03, bg = colors.red, gui = 'bold' } },
   inactive = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.gray, fg = colors.black},
-    c = {bg = colors.darkgray, fg = colors.white}
-  }
+    a = { fg = colors.base0, bg = colors.base02, gui = 'bold' },
+    b = { fg = colors.base03, bg = colors.base00 },
+    c = { fg = colors.base01, bg = colors.base02 },
+  },
 }
